8c6c15a70e279edc2834169b59afb461be2ea16a
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-25 14:32:38 -0500

    finish

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
  * YOUR CODE HERE *
  ******************/
 
+function onlyOdds(number){
+  const result = []
+  for(let i = 0; i < number.length; i++)
+  if(number[i] % 2 !== 0){
+    result.push(number[i])
+  }
+  return result
+}
 
+function onlyEvens(number){
+  const result = []
+  for(let i = 0; i < number.length; i++)
+  if(number[i] % 2 === 0){
+    result.push(number[i])
+  }
+  return result
+}
+function shortNamesOnly(name){
+  const result = []
+  for(let i = 0; i < name.length; i++)
+  if(name[i].length < 7){
+  result.push(name[i])
+  }return result
+}
 
-
-
+function dNames(name2){
+  const result = []
+  for(let i = 0; i < name2.length;i++)
+  if(name2[i].startsWith('D')){
+    result.push(name2[i])
+  }return result
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
